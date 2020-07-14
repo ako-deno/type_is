4aa425a5994a3c23c186a409109c5dd2153698d9
ngot <zhuanghengfei@gmail.com>
2020-07-14 20:00:25 +1000

    chore: fixed version deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { lookup } from "https://deno.land/x/media_types/mod.ts";
-export { parse } from "https://deno.land/x/content_type/mod.ts";
-export { test } from "https://deno.land/x/media_typer/mod.ts";
+export { lookup } from "https://deno.land/x/media_types@v2.4.1/mod.ts";
+export { parse } from "https://deno.land/x/content_type@1.0.1/mod.ts";
+export { test } from "https://deno.land/x/media_typer@1.0.1/mod.ts";
